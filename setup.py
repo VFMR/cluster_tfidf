29730b694b12dfb713bab9f38bb2ae187c8a7fe9
VFMR <reichv@ifo.de>
2022-01-19 13:44:41 +0100

    setting up env and setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+    name='cluster_tfidf',
+    version='1.0',
+    description='A module to compute the Cluster TFIDF method',
+    author='VFMR',
+    packages=['cluster_tfidf'],  #same as name
+    install_requires=['pandas', 'scikit-learn', 'numpy', 'tqdm'], #external packages as dependencies
+)
