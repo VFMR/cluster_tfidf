4d8f0267d708567f8a1fa734067cea9330fb0b4f
VFMR <reichv@ifo.de>
2023-03-16 16:49:52 +0100

    updated

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='cluster_tfidf',
-    version='0.1',
-    description='A module to compute the Cluster TFIDF method',
-    author='VFMR',
-    packages=['cluster_tfidf'],  #same as name
-    install_requires=['pandas', 
-                      'scikit-learn',
-                      'numpy',
-                      'tqdm'],
+    name="cluster_tfidf",
+    version="0.1",
+    description="A module to compute the Cluster TFIDF method",
+    author="VFMR",
+    packages=["cluster_tfidf"],  # same as name
+    install_requires=["pandas", "scikit-learn", "numpy", "tqdm"],
     extras_require={
-        'dev': [
-            'pytest',
-            'sphinx',
-            'sphinx_rtd_theme'
-            ]
-        }
+        "dev": ["pytest", "sphinx", "sphinx_rtd_theme", "pylint", "pre-commit"]
+    },
+    package_dir={
+        "": ".",
+    },
 )
